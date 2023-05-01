9d70ee6e1d60b090a13215b4330b5485ad9af907
Yasuhiro Shimodaira <89838961+yasu-sh@users.noreply.github.com>
2023-05-01 17:24:21 +0900

    Fixed mistakes.

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,9 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   Sys.setenv(RETICULATE_PYTHON = "C:/Python/venv/pytetrad/Scripts/")
-<<<<<<< HEAD
-
   # not good manner, just for working.
   setwd("./py-tetrad/pytetrad/")
-=======
->>>>>>> cef4c385927af3955493d2f8ec0ea96a969d1e81
 }
